2f78d446a9d745d0637084f5515160279ffd7255
omkarmanchekar <omkar.manchekar@somaiya.edu>
2022-02-24 11:48:57 -0500

    2

--- pyt.py
+++ pyt.py
@@ -1,11 +1,20 @@
+# import sys
+
+
+# s=sys.stdin.readlines()
+#     # print(i,end=" ")
+
+# print(s)
+
+# # [1,2,2,3,3,3]/n
+# # [23,3]/n
+# # (1,2)/n
+
+import fileinput
 import sys
 
-
-s=sys.stdin.readlines()
-    # print(i,end=" ")
-
-print(s)
-
-# [1,2,2,3,3,3]/n
-# [23,3]/n
-# (1,2)/n
+for line in fileinput.input():
+    # print(line,end=" ")
+    first = line.rstrip("[]'")
+    second=first.lstrip("[]'")
+    print(second)
